443aca16271f44d7e61e3a582b64226210a82072
RandomFlag <RandomFlag@users.noreply.github.com>
2019-01-16 04:54:15 +0200

    EditPage_Update

--- maine.py
+++ maine.py
@@ -47,7 +47,7 @@ def handle_text(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         markup.row('<', str(page.get(message.chat.id, 1)) + '/' + lastPage(comic.get(message.chat.id, '0')), '>')
         markup.row('/start')
-        bot.send_message(message.chat.id, findIMG(comic.get(message.chat.id, '0') + '/', page.get(message.chat.id, 1)), reply_markup=markup)
+        bot.edit_message_text(message.chat.id, findIMG(comic.get(message.chat.id, '0') + '/', page.get(message.chat.id, 1)), reply_markup=markup)
         
     #Inputting page
     elif state.get(message.chat.id, 'FindCom')  == 'InputPage':
